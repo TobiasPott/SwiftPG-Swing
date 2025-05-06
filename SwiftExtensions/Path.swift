554d06c9d13f098faea1e0b27dcf177bde0f7bf7
Tobias (iPad Pro) <mailme@tobiaspott.de>
2025-05-06 19:43:28 +0200

    Removed integer functions

--- SwiftExtensions/Path.swift
+++ SwiftExtensions/Path.swift
@@ -3,9 +3,4 @@ import SwiftUI
 extension Path {
     mutating func move(_ toX: CGFloat, _ toY: CGFloat) { self.move(to: .init(toX, toY)) }
     mutating func addLine(_ toX: CGFloat, _ toY: CGFloat) { self.addLine(to: .init(toX, toY)) }
-    
-    mutating func move2i(to: CGPoint) { self.move(to: .init(CGFloat(Int(to.x)),
-                                                            CGFloat(Int(to.y)))) }
-    mutating func addLine2i(to: CGPoint) { self.addLine(to: .init(CGFloat(Int(to.x)), 
-                                                                  CGFloat(Int(to.y)))) }
 }
